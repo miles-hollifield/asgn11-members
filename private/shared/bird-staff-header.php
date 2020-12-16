4c1d933eab464263b42af35ee553f06dd9939ad6
Miles <mileshollifieldgfp@gmail.com>
2020-12-15 23:03:38 -0500

    Modified bird-staff-header.php to identify user level

--- private/shared/bird-staff-header.php
+++ private/shared/bird-staff-header.php
@@ -15,9 +15,12 @@
     <header>
       <h1>Bird Staff Area</h1>
     </header>
+    
     <navigation>
       <ul>
         <?php if($session->is_logged_in()) { ?>
+        <li>User: <?php echo $session->username ?></li>
+        <li>User Level: <?php if($session->user_level == 'a'){echo 'Admin';} else{echo 'Member';} ?></li>
         <li><a href="<?php echo url_for('/bird-staff/index.php'); ?>">Menu</a></li>
         <li><a href="<?php echo url_for('/bird-staff/logout.php'); ?>">Logout</a></li>
         <?php } ?> 
