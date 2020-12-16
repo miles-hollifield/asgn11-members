be185c95e44084ac4d218370ff7b0434542b0c14
Miles <mileshollifieldgfp@gmail.com>
2020-12-15 22:58:07 -0500

    Modified classes/session.class.php to include user_level

--- private/classes/session.class.php
+++ private/classes/session.class.php
@@ -1,37 +1,78 @@
 <?php
-
 class Session {
-    private $admin_id;
 
-    public function __construct() {
-        session_start();
-        $this->check_stored_login();
+  private $admin_id;
+  public $username;
+  public $user_level;
+  private $last_login;
+
+  public const MAX_LOGIN_AGE = 60*60*24; // 1 day in seconds
+  
+  public function __construct() {
+    session_start();
+    $this->check_stored_login();
+  }
+  
+  public function login($admin) {
+    if($admin) {
+      // prevent session fixation attacks
+      session_regenerate_id();
+      $this->admin_id = $_SESSION['admin_id'] = $admin->id;
+      $this->username = $_SESSION['username'] = $admin->username;
+      $this->user_level = $_SESSION['user_level'] = $admin->user_level;
+      $this->last_login = $_SESSION['last_login'] = time();
     }
-
-     public function login($admin) {
-        // prevent against session fixation attacks 
-        if($admin) {
-            session_regenerate_id();
-            $_SESSION['admin_id'] = $admin->id;
-            $this->admin_id = $admin->id;
-            return true;
-        }
-     }
-
-     public function is_logged_in() {
-         return isset($this->admin_id);
-         
-     }
-
-     public function logout() {
-         unset( $_SESSION['admin_id']);
-         unset($this->admin_id);
-         return true;
-     }
-
-     private function check_stored_login() {
-         if(isset( $_SESSION['admin_id'])) {
-             $this->admin_id =  $_SESSION['admin_id'];
-         }
-     }
+    return true;
+  }
+  
+  public function is_logged_in() {
+    // return isset($this->admin_id);
+    return isset($this->admin_id) && $this->last_login_is_recent();
+  }
+  
+  public function logout() {
+    unset($_SESSION['admin_id']);
+    unset($_SESSION['username']);
+    unset($_SESSION['user_level']);
+    unset($_SESSION['last_login']);
+    unset($this->admin_id);
+    unset($this->username);
+    unset($this->user_level);
+    unset($this->last_login);
+    return true;
+  }
+  
+  private function check_stored_login() {
+    if(isset($_SESSION['admin_id'])) {
+      $this->admin_id = $_SESSION['admin_id'];
+      $this->username = $_SESSION['username'];
+      $this->user_level = $_SESSION['user_level'];
+      $this->last_login = $_SESSION['last_login'];
+    }
+  }
+  
+  private function last_login_is_recent() {
+    if(!isset($this->last_login)) {
+      return false;
+    } elseif(($this->last_login + self::MAX_LOGIN_AGE) < time()) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  public function message($msg="") {
+    if(!empty($msg)) {
+      // Then this is a "set" message
+      $_SESSION['message'] = $msg;
+      return true;
+    } else {
+      return $_SESSION['message'] ?? '';
+    }
+  }
+  
+  public function clear_message() {
+    unset($_SESSION['message']);
+  }
 }
+?>
